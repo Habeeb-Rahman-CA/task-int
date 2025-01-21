8ef8c5a27391a06b543eb698b62247329cfbc38a
HabeebR07 <habeebrahmanca07@gmail.com>
2025-01-21 18:47:51 +0530

    updates

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimations()]
